f9010df2a35a37b6497cb62809b65f78e8845028
ccjr <qianaiwuyu@gmail.com>
2022-07-14 15:47:18 +0800

    暂存

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+/** @jsxRuntime classic */
 import Didact from './Didact';
 
-const element = Didact.createElement(
-  'div',
-  { id: 'foo' },
-  Didact.createElement('a', null, 'bar'),
-  Didact.createElement('b')
+/** @jsx Didact.createElement */
+const element = (
+  <div>
+    <h1>
+      h1
+      <p>H1>p</p>
+      <a>H1>a</a>
+    </h1>
+    <h2>h2</h2>
+  </div>
 );
 
 const container = document.getElementById('root');
-
 Didact.render(element, container);
